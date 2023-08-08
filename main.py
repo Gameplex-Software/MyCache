ae513133debe40da1fc0ceb3d2085add12b021da
Tristan Poland <redstonecrafter126@gmail.com>
2023-08-07 23:00:59 -0400

    Working on triggering mysql client to display prompt

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import pymysql
-import redis.
-import time
 import socket
 import threading
+import mysql.connector
+import redis
+import time
 
 # MySQL database connection settings
 mysql_config = {
-    "host": "your_mysql_host",
-    "user": "your_mysql_user",
-    "password": "your_mysql_password",
-    "database": "your_mysql_database"
+    "host": "localhost",
+    "port": int("3306"),
+    "user": "admin",
+    "password": "gam3p13xs0ftwar3",
+    "database": "test"
 }
 
 # Redis cache configuration
@@ -22,17 +23,10 @@ min_frequency = 10  # Minimum number of requests per hour to consider caching
 cache_expiry = 3600  # Cache expiry time in seconds (1 hour)
 
 # Initialize MySQL and Redis connections
-mysql_connection = pymysql.connect(**mysql_config)
+mysql_connection = mysql.connector.connect(**mysql_config)
 redis_connection = redis.Redis(host=redis_host, port=redis_port, db=redis_db)
 
-def get_data_from_mysql(query):
-    cursor = mysql_connection.cursor()
-    cursor.execute(query)
-    data = cursor.fetchall()
-    cursor.close()
-    return data
-
-def get_data_with_caching(query):
+def get_data_with_caching(self, query):
     cached_data = redis_connection.get(query)
     
     if cached_data:
@@ -40,8 +34,8 @@ def get_data_with_caching(query):
         return cached_data.decode('utf-8')
     
     print("Fetching data from MySQL:")
-    data = get_data_from_mysql(query)
-    
+    data = self.get_data_from_mysql(query)
+        
     # Calculate the frequency of access for this query
     query_frequency_key = f"query_frequency:{query}"
     current_time = time.time()
@@ -55,7 +49,7 @@ def get_data_with_caching(query):
     
     return data
 
-def expire_old_cached_content():
+def expire_old_cached_content(self):
     current_time = time.time()
     # Find and remove old cached content
     for query in redis_connection.scan_iter(match="query:*"):
@@ -69,21 +63,47 @@ def expire_old_cached_content():
         redis_connection.zrem("query_frequencies", query_frequency_key)
 
 def handle_client(client_socket):
-    client_query = client_socket.recv(4096).decode('utf-8')
-    result = get_data_with_caching(client_query)
-    client_socket.sendall(str(result).encode('utf-8'))
+    # MySQL protocol handshake
+    client_handshake = client_socket.recv(1024)
+    client_socket.sendall(b'\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00')
 
-    # Update last access time for the query
-    redis_connection.set(f"last_access:{client_query}", time.time())
+    # MySQL protocol command
+    while True:
+        command_packet = client_socket.recv(1024)
+        if not command_packet:
+            break
         
-    # Perform cache cleanup
-    expire_old_cached_content()
+        # Extract the query from the command packet
+        query = command_packet[5:].decode('utf-8')
+        
+        result = get_data_with_caching(query)
+        
+        # MySQL protocol result
+        response_packet = construct_response_packet(result)
+        client_socket.sendall(response_packet)
+
+        # Update last access time for the query
+        redis_connection.set(f"last_access:{query}", time.time())
+        
+        # Perform cache cleanup
+        expire_old_cached_content()
 
     client_socket.close()
 
+def construct_response_packet(result):
+    result_length = len(result)
+    response_packet = bytearray()
+    response_packet.extend((result_length + 7).to_bytes(3, byteorder='little'))  # Packet length
+    response_packet.extend(b'\x00\x00\x00\x02')  # Sequence number
+    response_packet.extend((result_length + 1).to_bytes(3, byteorder='little'))  # Length of string
+    response_packet.extend(b'\x00')  # Status
+    response_packet.extend(result.encode('utf-8'))  # Result
+
+    return response_packet
+
 if __name__ == "__main__":
     proxy_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    proxy_server.bind(("0.0.0.0", 3306))
+    proxy_server.bind(("0.0.0.0", 3308))
     proxy_server.listen(5)
 
     try:
